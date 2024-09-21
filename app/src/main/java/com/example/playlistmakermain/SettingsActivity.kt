a8902d72a5ab045e0b029e6dcf233c440f2f68d0
papcha07 <legoman056@gmail.com>
2024-09-21 16:51:58 +0700

    Commit 2. Спринт 9

--- app/src/main/java/com/example/playlistmakermain/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmakermain/SettingsActivity.kt
@@ -15,7 +15,7 @@ class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        setContentView(R.layout.settings)
 
         val backButton = findViewById<ImageView>(R.id.back)
 
@@ -25,6 +25,56 @@ class SettingsActivity : AppCompatActivity() {
             startActivity(backIntent)
 
         }
+
+
+        val shareButtonId = findViewById<LinearLayout>(R.id.shareButton)
+        shareButtonId.setOnClickListener{
+
+            val sendIntent = Intent(Intent.ACTION_SEND)
+            sendIntent.type = "text/plain"
+            sendIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.android_course_url))
+            startActivity(sendIntent)
+
+        }
+
+
+        val reportButtonId = findViewById<LinearLayout>(R.id.reportButton)
+        reportButtonId.setOnClickListener{
+
+
+            val reportIntent = Intent(Intent.ACTION_SEND)
+            reportIntent.setType("message/rfc822")
+
+
+            reportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("legoman056@gmail.com"))
+            reportIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.subject_text))
+            reportIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.text_to_developers))
+
+            startActivity(reportIntent)
+        }
+
+
+        val documentsButtonId = findViewById<LinearLayout>(R.id.documentsId)
+
+        documentsButtonId.setOnClickListener{
+
+
+            val browserIntent = Intent(Intent.ACTION_VIEW,Uri.parse(getString(R.string.offer)))
+            startActivity(browserIntent)
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
 
     }
 }
