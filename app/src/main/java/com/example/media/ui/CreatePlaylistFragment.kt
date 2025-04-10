6e3a3a36c9586df884ec074820db45441db2aae7
Vsevolod Sazonov <legoman056@gmail.com>
2025-04-10 23:04:49 +0700

    сделал экран фрагмента с созданием альбома

---
+++ app/src/main/java/com/example/media/ui/CreatePlaylistFragment.kt
@@ -0,0 +1,24 @@
+package com.example.media.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.playlistmakermain.R
+
+
+class CreatePlaylistFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_create_playlist, container, false)
+    }
+
+}
